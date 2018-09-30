0a7588105b44705deab058cc8994cfecac9f72cd
Halden Lin <halden.lin@gmail.com>
2018-09-30 15:15:10 -0700

    add css-module processing

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import * as ReactDOM from 'react-dom';
 import App from './app/App';
 import './index.css';
 
-declare var Module: any;
-
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement,
