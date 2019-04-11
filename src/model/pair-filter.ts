56d3c34f4d695f9975633480538414c427e84a74
Halden Lin <halden.lin@gmail.com>
2019-04-11 16:47:15 -0700

    Add unsat filter.

--- src/model/pair-filter.ts
+++ src/model/pair-filter.ts
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import { ConstraintMapObject } from './constraints';
-import { Pair, PairEval, PairObject } from './pair';
+import { Pair, PairEval, PairEvalType, PairObject } from './pair';
 
 export class PairFilter {
   static getTypesFromString(s: string): PairFilterType[] {
@@ -12,6 +12,9 @@ export class PairFilter {
           break;
         case 'is:passing':
           filterTypes.push(PairFilter.BY_PASS_TYPE);
+          break;
+        case 'is:unsat':
+          filterTypes.push(PairFilter.BY_UNSAT_TYPE)
           break;
       }
       return filterTypes;
@@ -26,24 +29,35 @@ export class PairFilter {
         return PairFilter.byPass;
       case PairFilter.BY_FAIL_TYPE:
         return PairFilter.byFail;
+      case PairFilter.BY_UNSAT_TYPE:
+        return PairFilter.byUnsat;
     }
   }
 
   static BY_PASS_TYPE: 'bypass' = 'bypass';
   static byPass(pair: PairObject, opt?: { constraintMap: ConstraintMapObject }): boolean {
-    if (_.isUndefined(opt) || _.isUndefined(opt.constraintMap)) {
-      throw new Error('PairFilter.byPass requires a constraintMap in opt.');
-    }
-    return Pair.getEval(pair, opt.constraintMap) === PairEval.PASS;
+    return byEvalType(pair, PairEval.PASS, opt);
   }
 
   static BY_FAIL_TYPE: 'byfail' = 'byfail';
   static byFail(pair: PairObject, opt?: { constraintMap: ConstraintMapObject }): boolean {
-    if (_.isUndefined(opt) || _.isUndefined(opt.constraintMap)) {
-      throw new Error('PairFilter.byFail requires a constraintMap in opt.');
-    }
-    return Pair.getEval(pair, opt.constraintMap) === PairEval.FAIL;
+    return byEvalType(pair, PairEval.FAIL, opt);
+  }
+
+  static BY_UNSAT_TYPE: 'byunsat' = 'byunsat';
+  static byUnsat(pair: PairObject, opt?: { constraintMap: ConstraintMapObject }): boolean {
+    return byEvalType(pair, PairEval.UNSAT, opt);
   }
 }
 
-export type PairFilterType = typeof PairFilter.BY_PASS_TYPE | typeof PairFilter.BY_FAIL_TYPE;
+function byEvalType(pair: PairObject, evalType: PairEvalType, opt?: { constraintMap: ConstraintMapObject }): boolean {
+  if (_.isUndefined(opt) || _.isUndefined(opt.constraintMap)) {
+    throw new Error('Filter requires a constraintMap in opt.');
+  }
+  return Pair.getEval(pair, opt.constraintMap) === evalType;
+}
+
+export type PairFilterType =
+  typeof PairFilter.BY_PASS_TYPE |
+  typeof PairFilter.BY_FAIL_TYPE |
+  typeof PairFilter.BY_UNSAT_TYPE;
