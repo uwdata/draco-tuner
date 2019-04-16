d43a0e273951f15f0c268f8dc43444bb44d433fb
Halden Lin <halden.lin@gmail.com>
2019-04-16 16:21:33 -0700

    Remove unused files.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,26 +4,17 @@ import * as dracoActions from './draco-actions';
 import * as dracoWorkerActions from './draco-worker-actions';
 import * as pairCollectionActions from './pair-collection-actions';
 import * as textEditorActions from './text-editor-actions';
-import * as tunerActions from './tuner-actions';
 
 export { pairCollectionActions };
-export { tunerActions };
 export { dracoWorkerActions };
 export { dracoActions };
 export { appActions };
 export { textEditorActions };
 
-export type TunerAction = ActionType<typeof tunerActions>;
 export type PairCollectionAction = ActionType<typeof pairCollectionActions>;
 export type DracoWorkerAction = ActionType<typeof dracoWorkerActions>;
 export type DracoAction = ActionType<typeof dracoActions>;
 export type AppAction = ActionType<typeof appActions>;
 export type TextEditorAction = ActionType<typeof textEditorActions>;
 
-export type RootAction =
-  | TunerAction
-  | PairCollectionAction
-  | DracoWorkerAction
-  | DracoAction
-  | AppAction
-  | TextEditorAction;
+export type RootAction = PairCollectionAction | DracoWorkerAction | DracoAction | AppAction | TextEditorAction;
--- src/actions/tuner-actions.ts
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'typesafe-actions';
-
-export const updateDracoSolutionSet = createAction('draco/UPDATE_DRACO_SOLUTION_SET', resolve => {
-  return (code: string) => resolve({ code }, { WebWorker: true });
-});
